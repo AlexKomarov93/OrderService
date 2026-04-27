253e92379f15c95045dad43216affae5f9e5fa98
AlexKomarov93 <alexfurmanov30@gmail.com>
2026-04-27 17:08:15 +0300

    Добавлены методы existsByUsername и existsByEmail в UserRepository

--- src/main/java/com/example/OrderService/repos/UserRepository.java
+++ src/main/java/com/example/OrderService/repos/UserRepository.java
@@ -4,7 +4,11 @@ import com.example.OrderService.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
-    Users findByUsername(String username);
+    Optional<Users> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
