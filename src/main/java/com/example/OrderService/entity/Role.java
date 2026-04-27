206e043a2332b60cd9f107ec97097cf74bdcf923
AlexKomarov93 <alexfurmanov30@gmail.com>
2026-04-27 13:17:42 +0300

    Role перемещен в папку enums

--- src/main/java/com/example/OrderService/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.OrderService.entity;
-
-public enum Role {
-    USER,ADMIN
-}
---
+++ src/main/java/com/example/OrderService/entity/enums/Role.java
@@ -0,0 +1,5 @@
+package com.example.OrderService.entity.enums;
+
+public enum Role {
+    USER,ADMIN
+}
