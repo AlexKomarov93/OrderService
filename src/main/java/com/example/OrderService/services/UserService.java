11ec370dc695e35181563f281366881190628657
AlexKomarov93 <alexfurmanov30@gmail.com>
2026-04-27 16:56:22 +0300

    Добавлен интерфейс UserService

--- src/main/java/com/example/OrderService/services/UserService.java
+++ src/main/java/com/example/OrderService/services/UserService.java
@@ -1,11 +1,14 @@
 package com.example.OrderService.services;
 
-import com.example.OrderService.entity.Users;
+import com.example.OrderService.dto.TokenResponseDto;
+import com.example.OrderService.dto.UsersResponseDto;
 
 public interface UserService {
-    Users registerUser(String username, String password, String email);
+
+    UsersResponseDto registerUser(String username, String password, String email);
 
     TokenResponseDto login(String username, String password);
 
     TokenResponseDto refreshToken(String oldRefreshToken);
+
 }
