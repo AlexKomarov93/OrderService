1e5b1ea0dedc016f741a673d713d1d9edc48b3db
AlexKomarov93 <alexfurmanov30@gmail.com>
2026-04-29 10:28:56 +0300

    Добавлен контроллер аутентификации с эндпоинтами регистрации и входа

---
+++ src/main/java/com/example/OrderService/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.example.OrderService.controller;
+
+import com.example.OrderService.dto.request.SignInRequest;
+import com.example.OrderService.dto.request.SignUpRequest;
+import com.example.OrderService.dto.response.JwtAuthenticationResponse;
+import com.example.OrderService.services.authService.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/reg")
+    public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        return authenticationService.signUp(request);
+    }
+
+    @PostMapping("/login")
+    public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
+        return authenticationService.signIn(request);
+    }
+}
+
+
+
