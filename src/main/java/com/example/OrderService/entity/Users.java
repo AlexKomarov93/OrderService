b5042fffb3672179ccde59308ed4a0dabf89c37e
AlexKomarov93 <alexfurmanov30@gmail.com>
2026-05-21 11:38:42 +0300

    Исправлены импорты в сущности Users

--- src/main/java/com/example/OrderService/entity/Users.java
+++ src/main/java/com/example/OrderService/entity/Users.java
@@ -1,8 +1,18 @@
 package com.example.OrderService.entity;
 
 import com.example.OrderService.entity.enums.Role;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
